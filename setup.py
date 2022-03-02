a9445901db373dffcc7fc03c6be3d2b07ad9eba5
Bert Van Staeyen <71755551+bertvanstaeyen@users.noreply.github.com>
2022-03-02 12:38:31 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Chess AI',
+      author='Bert Van Staeyen',
+      author_email='bertvanstaeyen@gmail.com',
+      packages=['distutils', 'distutils.command', 'numpy'],
+     )
